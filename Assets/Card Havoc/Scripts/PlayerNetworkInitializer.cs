1e05de8a60c724e548329a0badb83ec67a6c81aa
Carson French <frenchcarson3@gmail.com>
2025-04-15 23:30:21 -0500

    removed uneed fields

--- Assets/Card Havoc/Scripts/PlayerNetworkInitializer.cs
+++ Assets/Card Havoc/Scripts/PlayerNetworkInitializer.cs
@@ -10,10 +10,6 @@ public class PlayerNetworkInitializer : NetworkBehaviour
     [Header("Local Player Only")]
     [SerializeField] private GameObject mainCamera;         // Your camera with CinemachineBrain
     [SerializeField] private GameObject virtualCamera;         // Your camera with CinemachineBrain
-    [SerializeField] private GameObject fpsGraphics;        // Arms, weapon, etc.
-    
-    [Header("Remote Players Only")]
-    [SerializeField] private GameObject thirdPersonGraphics; // Full-body mesh for other players
 
     [Header("Scripts")]
     [SerializeField] private MonoBehaviour[] localOnlyScripts; // e.g., PlayerController, CameraController
@@ -25,10 +21,6 @@ public class PlayerNetworkInitializer : NetworkBehaviour
 
         if (IsOwner)
         {
-            //TODO: Having trouble not switching camreas... Maybe flip logic idk.
-            // I think it has to do with the Cinemachine brain and what not look into that more
-            //      Find a way to diable everything but the FPSgraphics of the other player
-
             GameObject worldCam = GameObject.FindGameObjectWithTag("WorldCamera");
             if (worldCam != null)
                 worldCam.SetActive(false);
@@ -42,27 +34,14 @@ public class PlayerNetworkInitializer : NetworkBehaviour
         else
         {
 
-            //gameObject.GetComponent<PlayerController>().enabled = false;
-
-
-
             // Remote player setup
             if (virtualCamera != null)
                 virtualCamera.SetActive(false);
                 Debug.Log("SET cirtual FALSE");
 
-            
             if (mainCamera != null)
                 mainCamera.SetActive(false);
                 Debug.Log("SET main camrea FALSE");
-
-            if (fpsGraphics != null)
-                fpsGraphics.SetActive(false);
-                Debug.Log("SET fps FALSE");
-
-            if (thirdPersonGraphics != null)
-                thirdPersonGraphics.SetActive(true);
-                Debug.Log("SET Thirdperson true");
 
             foreach (var script in localOnlyScripts)
             {
